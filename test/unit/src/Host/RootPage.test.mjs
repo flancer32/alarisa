0692977550578f2fea0fe1a9571b029faefeef87
Alex Gusev <alex@flancer64.com>
2026-07-16 13:19:00 +0300

    Add root favicon

--- test/unit/src/Host/RootPage.test.mjs
+++ test/unit/src/Host/RootPage.test.mjs
@@ -5,8 +5,15 @@ import test from "node:test";
 test("root page requires an explicit desk or mob choice", async () => {
   const html = await fs.readFile(new URL("../../../../web/index.html", import.meta.url), "utf8");
 
+  assert.match(html, /rel="icon" href="\/favicon\.ico"/);
   assert.match(html, /href="\/desk\/"/);
   assert.match(html, /href="\/mob\/"/);
   assert.doesNotMatch(html, /http-equiv=["']refresh/i);
   assert.doesNotMatch(html, /<script\b/i);
+});
+
+test("root page favicon is an ICO resource", async () => {
+  const icon = await fs.readFile(new URL("../../../../web/favicon.ico", import.meta.url));
+
+  assert.deepEqual([...icon.subarray(0, 4)], [0, 0, 1, 0]);
 });
